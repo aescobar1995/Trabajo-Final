664c9598d2e9641d429bd8af09727111e3430418
aescobar95 <agustin.escobarr95@gmail.com>
2022-08-02 12:12:09 -0300

    Creados los modelos criticos y binger

--- AESeries/usuarios/models.py
+++ AESeries/usuarios/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from email.policy import default
+from tokenize import blank_re
 
-# Create your models here.
+class Binger(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    alias = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Binger {self.nombre} {self.apellido}"
+
+
+class Critico(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    alias = models.CharField(max_length=20)
+    experiencia = models.TextField()
+
+    def __str__(self):
+        return f"Critico {self.nombre} {self.apellido}"
