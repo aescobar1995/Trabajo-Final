52011d72adc9da6059277f91d80c6efad0ff8a23
aescobar95 <agustin.escobarr95@gmail.com>
2022-08-02 11:37:16 -0300

    Comienzo del proyecto

--- AESeries/AESeries/settings.py
+++ AESeries/AESeries/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)u=q5(-uws)-iv9xp27*!#bp^i)go^wokhk@sz73-d81k2!)j@'
+SECRET_KEY = 'django-insecure-pa$ob(54vh=^o)u5tcwb)b5gnm$zagskfelxex3qb!ebidot0y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
